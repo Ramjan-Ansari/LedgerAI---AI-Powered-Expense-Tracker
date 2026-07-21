f5c808be2302a6d76c22b2b0d67ff1c465bb54d7
Ramjan Ansari <ramjanansari32426@gmail.com>
2026-07-21 15:08:27 +0530

    feat: create dashboard UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,22 @@ import Layout from './components/Layout.jsx'
 import Register from './pages/Register.jsx';
 import Login from './pages/Login.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={ <Login /> } />
       <Route path='/register' element={ <Register /> } />
-      <Route path='/' element={
-        <ProtectedRoute >
-          <Layout /> 
-        </ProtectedRoute>
-      } />
+      <Route 
+        element={
+          <ProtectedRoute >
+            <Layout /> 
+          </ProtectedRoute>
+        } 
+      >
+        <Route path='/' element={ <Dashboard /> } />
+      </Route>
     </Routes>
   )
 }
